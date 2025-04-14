b56ceed5e5354dccb5efa90371f90143b265ca9d
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-04-14 13:41:51 -0300

    projeto craido

---
+++ src/main/java/com/CadastroViagem/CadastroViagemApplication.java
@@ -0,0 +1,13 @@
+package com.CadastroViagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroViagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroViagemApplication.class, args);
+	}
+
+}
